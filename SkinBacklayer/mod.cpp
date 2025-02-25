858ec35c12150ba841aa1a992b3cc6d415f4b6a8
Azuki3 <132030806+AzukiSan-png@users.noreply.github.com>
2025-02-25 16:32:27 +0900

    Update mod.cpp

--- SkinBacklayer/mod.cpp
+++ SkinBacklayer/mod.cpp
@@ -1,10 +1,11 @@
 #include "helpers.h"
+#include <cstdint>
 
 extern "C" {
     void __declspec(dllexport) Init() {
-        WRITE_MEMORY(0x14027808A, unsigned char, 0x03);
-        WRITE_MEMORY(0x1402781DB, unsigned char, 0xF2);
-        WRITE_MEMORY(0x151300C71, unsigned char, 0xF3);
-        WRITE_MEMORY(0x151300CC2, unsigned char, 0xF2);
+        WRITE_MEMORY(0x14027808A, uint8_t, 0x03);
+        WRITE_MEMORY(0x1402781DB, uint8_t, 0xF2);
+        WRITE_MEMORY(0x151300C71, uint8_t, 0xF3);
+        WRITE_MEMORY(0x151300CC2, uint8_t, 0xF2);
     }
 }
